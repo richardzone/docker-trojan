f1a98d7d7b9d5ce811f188744ca6fe96fc3bd6ad
Richard LIU <i@richard.zone>
2021-01-28 04:03:01 +0800

    print trojan version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,5 +16,8 @@ fi
 
 sed -i '' -e "s/CERT_PATH_TO_REPLACE/$CERT_PATH/" -e "s/KEY_PATH_TO_REPLACE/$KEY_PATH/" -e "s/PASSWORD_TO_REPLACE/$PASSWORD/" trojan.json
 
+# print Trojan version
+trojan -v
+
 # Run Trojan Server
 exec "$@"
